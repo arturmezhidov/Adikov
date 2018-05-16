fdfd454a1b2b163f2a002e0882a8579b623ac39f
Artur Mezhidov <arturrasulovich@gmail.com>
2018-05-16 11:15:05 +0300

    Fixed blog icon.

--- Adikov/Adikov/Services/SidebarService.cs
+++ Adikov/Adikov/Services/SidebarService.cs
@@ -115,7 +115,7 @@ namespace Adikov.Services
             SidebarGroup item = new SidebarGroup
             {
                 Text = "Блог",
-                Icon = "icon-info",
+                Icon = "icon-notebook",
                 ViewLink = "/Blog"
             };
 
